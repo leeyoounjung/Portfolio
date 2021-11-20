f8ce1822161df3dd5570c2af3889282588003ae8
leeyoounjung <isjj9905@gmail.com>
2021-11-20 19:19:27 +0900

    코딩작업 04

--- Portfolio.js
+++ Portfolio.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
-
+    $(".paper > .right-btn").click(function(){
+        $(".paper02").addClass("active");
+      });
+      $(".paper02 > .left-btn").click(function(){
+        $(".paper02").toggleClass("active");
+      });
+      $(".paper02 > .right-btn").click(function(){
+        $(".paper03").addClass("active");
+      });
+      $(".paper03 > .left-btn").click(function(){
+        $(".paper03").toggleClass("active");
+      });
 });
