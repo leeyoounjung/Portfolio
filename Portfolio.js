a0741cee2af9f47a4866de774f3b018a73736fd0
leeyoounjung <isjj9905@gmail.com>
2021-11-26 22:47:35 +0900

    코딩작업 08

--- Portfolio.js
+++ Portfolio.js
@@ -1,34 +1,40 @@
 $(document).ready(function(){
-    $(".paper > .right-btn").click(function(){
-        $(".paper02").addClass("active");
-      });
-      $(".paper02 > .left-btn").click(function(){
-        $(".paper02").toggleClass("active");
-      });
-      $(".paper02 > .right-btn").click(function(){
-        $(".paper03").addClass("active");
-      });
-      $(".paper03 > .left-btn").click(function(){
-        $(".paper03").toggleClass("active");
-      });
-
-        
- $(".slider").each(function (index, node) {
-  var $group = $(node);
-  var $content = $group.find(" > .content");
-
-  gsap.to($content, {
-      xPercent: -100 * ($content.length - 1),
-      ease: "none",
-      scrollTrigger: {
-          trigger: $group,
-          start: "top top",
-          end: "+=" + ($content.length - 1) + "00%",
-          pin: true,
-          scrub: true,
-          markers:true
-      }
+  $(".paper > .right-btn").click(function(){
+    $(".paper02").addClass("active");
   });
+  $(".paper02 > .left-btn").click(function(){
+    $(".paper02").toggleClass("active");
+  });
+  $(".paper02 > .right-btn").click(function(){
+    $(".paper03").addClass("active");
+  });
+  $(".paper03 > .left-btn").click(function(){
+    $(".paper03").toggleClass("active");
+  });
+  
+ $(".slider").each(function (index, node) {
+        var $group = $(node);
+        var $content = $group.find(" > .content");
+
+        gsap.to($content, {
+            xPercent: -100 * ($content.length - 1),
+            ease: "none",
+            scrollTrigger: {
+                trigger: $group,
+                start: "top top",
+                end: "+=" + ($content.length - 1) + "00%",
+                pin: true,
+                scrub: true,
+                markers:true
+            }
+        });
+    });
+   
+
+  
 });
 
-});
+  function fnMove(seq){
+        var offset = $(".content" + seq).offset();
+        $('html, body').animate({scrollTop : offset.top}, 400);
+    }
